0b1154d309b585451ef09d2156eb08ffea9f1b9f
HollowayX <ousjkgpbma460@hotmail.com>
2025-10-22 12:21:45 +0800

    update dpeloy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const confidentialGold = await deploy("ConfidentialGOLD", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`ConfidentialGOLD contract: `, confidentialGold.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_confidentialGold"; // id required to prevent reexecution
+func.tags = ["ConfidentialGOLD"];
