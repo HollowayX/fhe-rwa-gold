b5920a6c4d9a3da47321717d22385965098fb682
HollowayX <ousjkgpbma460@hotmail.com>
2025-10-22 12:38:39 +0800

    fix confidential gold: total zama

--- contracts/ConfidentialGOLD.sol
+++ contracts/ConfidentialGOLD.sol
@@ -3,18 +3,18 @@ pragma solidity ^0.8.27;
 
 import {ConfidentialFungibleToken} from "new-confidential-contracts/token/ConfidentialFungibleToken.sol";
 import {SepoliaConfig} from "@fhevm/solidity/config/ZamaConfig.sol";
-import {FHE, euint64} from "@fhevm/solidity/lib/FHE.sol";
+import {FHE, euint64, externalEuint64} from "@fhevm/solidity/lib/FHE.sol";
 
 contract ConfidentialGOLD is ConfidentialFungibleToken, SepoliaConfig {
     constructor() ConfidentialFungibleToken("cGOLD", "cGOLD", "") {}
 
-    function mint(address to, uint64 amount) public {
-        euint64 encryptedAmount = FHE.asEuint64(amount);
-        _mint(to, encryptedAmount);
+    function mint(address to, externalEuint64 encryptedAmount, bytes calldata inputProof) public {
+        euint64 amount = FHE.fromExternal(encryptedAmount, inputProof);
+        _mint(to, amount);
     }
 
-    function burn(address from, uint64 amount) public {
-        euint64 encryptedAmount = FHE.asEuint64(amount);
-        _burn(from, encryptedAmount);
+    function burn(address from, externalEuint64 encryptedAmount, bytes calldata inputProof) public {
+        euint64 amount = FHE.fromExternal(encryptedAmount, inputProof);
+        _burn(from, amount);
     }
 }
