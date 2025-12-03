16a89258606506df82424960fc575c56f9cf2f66
HollowayX <ousjkgpbma460@hotmail.com>
2025-12-03 12:18:24 +0800

    zama update

--- contracts/ConfidentialGOLD.sol
+++ contracts/ConfidentialGOLD.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: BSD-3-Clause-Clear
 pragma solidity ^0.8.27;
 
-import {ConfidentialFungibleToken} from "new-confidential-contracts/token/ConfidentialFungibleToken.sol";
-import {SepoliaConfig} from "@fhevm/solidity/config/ZamaConfig.sol";
+
+import {ERC7984} from "confidential-contracts-v91/contracts/token/ERC7984/ERC7984.sol";
+import {ZamaEthereumConfig} from "@fhevm/solidity/config/ZamaConfig.sol";
 import {FHE, euint64, externalEuint64} from "@fhevm/solidity/lib/FHE.sol";
 
-contract ConfidentialGOLD is ConfidentialFungibleToken, SepoliaConfig {
-    constructor() ConfidentialFungibleToken("cGOLD", "cGOLD", "") {}
+contract ConfidentialGOLD is ERC7984, ZamaEthereumConfig {
+    constructor() ERC7984("cGOLD", "cGOLD", "") {}
 
     function mint(address to, externalEuint64 encryptedAmount, bytes calldata inputProof) public {
         euint64 amount = FHE.fromExternal(encryptedAmount, inputProof);
